e294793f4b7bbf5ade4c53014a91c4e8c9938643
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-11-04 02:01:58 -0800

    Save/Load of ranked filter settings

--- Source/StorageSettingsRankComp.cs
+++ Source/StorageSettingsRankComp.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Verse;
 using RimWorld;
+using Harmony;
 
 namespace Stockpile_Ranking
 {
@@ -17,11 +18,13 @@ namespace Stockpile_Ranking
 			return Current.Game.GetComponent<RankComp>().rankedSettings;
 		}
 
-		public static List<ThingFilter> GetRanks(StorageSettings settings)
+		public static List<ThingFilter> GetRanks(StorageSettings settings, bool create = true)
 		{
 			var dict = Get();
 			if (dict.TryGetValue(settings, out List<ThingFilter> list))
 				return list;
+			else if (!create)
+				return null;
 			List<ThingFilter> newList = new List<ThingFilter>();
 			dict[settings] = newList;
 			return newList;
@@ -46,6 +49,30 @@ namespace Stockpile_Ranking
 		{
 			if (rank == 0) return;//sanity check
 			GetRanks(settings).RemoveAt(rank - 1);
+		}
+	}
+
+	[HarmonyPatch(typeof(StorageSettings), "ExposeData")]
+	static class ExposeData
+	{
+		//public void ExposeData()
+		public static void Postfix(StorageSettings __instance)
+		{
+			if (Scribe.mode == LoadSaveMode.Saving)
+			{
+				List<ThingFilter> ranks = RankComp.GetRanks(__instance, false);
+				if (ranks == null) return;
+
+				Scribe_Collections.Look(ref ranks, "ranks", LookMode.Deep);
+				return;
+			}
+			List<ThingFilter> loadRanks = null;
+
+			Scribe_Collections.Look(ref loadRanks, "ranks", LookMode.Deep);
+			if (loadRanks == null) return;
+
+			foreach (ThingFilter rank in loadRanks)
+				RankComp.AddFilter(__instance, rank);
 		}
 	}
 }
