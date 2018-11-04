8a86b11b2716489ebda67835b69b78406bde08c9
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-11-04 05:19:36 -0800

    Transpiler for AllowedToAccept set up, pass-thru now

--- Source/StorageSettingsPatches.cs
+++ Source/StorageSettingsPatches.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Reflection;
+using System.Reflection.Emit;
 using Verse;
 using RimWorld;
 using Harmony;
@@ -39,6 +41,33 @@ namespace Stockpile_Ranking
 		public static void Prefix(StorageSettings __instance, StorageSettings other)
 		{
 			RankComp.CopyFrom(__instance, other);
+		}
+	}
+
+	[HarmonyPatch(typeof(StorageSettings), "AllowedToAccept", new Type[] { typeof(Thing)})]
+	class AllowedToAccept_Thing
+	{
+		//public bool AllowedToAccept(Thing t)
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			FieldInfo filterInfo = AccessTools.Field(typeof(StorageSettings), "filter");
+
+			foreach(CodeInstruction i in instructions)
+			{
+				if(i.opcode == OpCodes.Ldfld && i.operand == filterInfo)
+				{
+					//instead of this.filter.Allows(t)
+					//GetFilter(this).Allows(t)
+					yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(AllowedToAccept_Thing), nameof(GetFilter)));
+				}
+				else
+					yield return i;
+			}
+		}
+
+		public static ThingFilter GetFilter(StorageSettings settings)
+		{
+			return settings.filter;
 		}
 	}
 }
