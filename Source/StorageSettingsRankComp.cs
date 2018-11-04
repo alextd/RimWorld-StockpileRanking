189bb2c67b5a4886c7657ec740d3e5b3f13b3562
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-11-04 04:49:55 -0800

    moving files around

--- Source/StorageSettingsRankComp.cs
+++ Source/StorageSettingsRankComp.cs
@@ -51,28 +51,4 @@ namespace Stockpile_Ranking
 			GetRanks(settings).RemoveAt(rank - 1);
 		}
 	}
-
-	[HarmonyPatch(typeof(StorageSettings), "ExposeData")]
-	static class ExposeData
-	{
-		//public void ExposeData()
-		public static void Postfix(StorageSettings __instance)
-		{
-			if (Scribe.mode == LoadSaveMode.Saving)
-			{
-				List<ThingFilter> ranks = RankComp.GetRanks(__instance, false);
-				if (ranks == null) return;
-
-				Scribe_Collections.Look(ref ranks, "ranks", LookMode.Deep);
-				return;
-			}
-			List<ThingFilter> loadRanks = null;
-
-			Scribe_Collections.Look(ref loadRanks, "ranks", LookMode.Deep);
-			if (loadRanks == null) return;
-
-			foreach (ThingFilter rank in loadRanks)
-				RankComp.AddFilter(__instance, rank);
-		}
-	}
 }
