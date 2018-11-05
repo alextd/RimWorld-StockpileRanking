72cd97540772476d3f4cdc846bcc0e348bfcf350
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-11-05 03:32:09 -0800

    Clearer setting

--- Source/Settings.cs
+++ Source/Settings.cs
@@ -21,10 +21,10 @@ namespace Stockpile_Ranking
 			options.Begin(wrect);
 
 			bool old = returnLower;
-			options.CheckboxLabeled("Return items when higher ranked items become available", ref returnLower);
+			options.CheckboxLabeled("Make sure items are removed when higher ranked items become available", ref returnLower);
 			if (old != returnLower)
 				RankComp.Get().dirty = true;
-			options.Label("This also will require more processing time. Disable it if there seems to be a problem.");
+			options.Label("This also will require more processing time. Disable it if there seems to be a slowdown problem.");
 			options.Label("This assumes a stockpile with lower priority with space to move lower-ranked items to.");
 			options.Label("Items would normally be returned when there is a spot in the ranked stockpile open (Not by design but due to internal cache)");
 			options.Label("(technically speaking, this option is a toggle: comparing ranked filters either to all things or only the things marked that they need hauling. The end result is that things don't know they need to be returned since the hauling system doesn't know about ranked stockpiles)");
