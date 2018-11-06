c1289886d843feaccb5e24cf7a7c1d48ad00d398
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-11-06 05:51:11 -0800

    Changing quality/HP filters calls the notify action

---
+++ Source/NotifyChangedQualityHP.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using Verse;
+using RimWorld;
+using Harmony;
+
+namespace Stockpile_Ranking
+{
+	//Fleshing out vanilla functions: changing Quality and HP filter calls notify method
+	[HarmonyPatch(typeof(ThingFilterUI), "DrawHitPointsFilterConfig")]
+	static class DrawHitPointsFilterConfig
+	{
+		//private static void DrawHitPointsFilterConfig(ref float y, float width, ThingFilter filter)
+		public static void Prefix(ThingFilter filter, ref FloatRange __state)
+		{
+			__state = filter.AllowedHitPointsPercents;
+		}
+		public static void Postfix(ThingFilter filter, FloatRange __state)
+		{
+			if (__state != filter.AllowedHitPointsPercents)
+				(RankComp.settingsChangedCallbackInfo.GetValue(filter) as Action)();
+		}
+	}
+
+	[HarmonyPatch(typeof(ThingFilterUI), "DrawQualityFilterConfig")]
+	static class DrawQualityFilterConfig
+	{
+		//private static void DrawQualityFilterConfig(ref float y, float width, ThingFilter filter)
+		public static void Prefix(ThingFilter filter, ref QualityRange __state)
+		{
+			__state = filter.AllowedQualityLevels;
+		}
+		public static void Postfix(ThingFilter filter, QualityRange __state)
+		{
+			if (__state != filter.AllowedQualityLevels)
+				(RankComp.settingsChangedCallbackInfo.GetValue(filter) as Action)();
+		}
+	}
+}
